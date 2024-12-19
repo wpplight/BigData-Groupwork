590fb21438f8a5938b0d6c4d6943f377c054b68d
wpplight <2023040093@buct.edu.cn>
2024-12-20 04:02:59 +0800

    示范

--- setting-code/javascrip/index.js
+++ setting-code/javascrip/index.js
@@ -1,11 +1,6 @@
 // 选择所有具有 'card' 类的按钮
 var resultPtr;
 const buttons = document.querySelectorAll('.card');
-Module.onRuntimeInitialized = function () {
-        // 现在运行时已经准备好
-         resultPtr = _get();  // 安全地调用本地函数
-        
-};
 var mm;  
 function shuaxin()
 {
